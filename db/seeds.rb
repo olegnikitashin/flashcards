160f735266c637f591cd541d0259ea83123dc8b2
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-08-03 18:10:32 +0300

    Optimise db:seed to eliminate repeats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -16,13 +15,19 @@ require 'open-uri'
 
   begin
     page = Nokogiri::HTML(open(page_url))
-    word_parse = page.css('div.jsn-article-content table tbody tr.rowA td.bigLetter') + page.css('div.jsn-article-content table tbody tr.rowB td.bigLetter')
-    description_parse = page.css('div.jsn-article-content table tbody tr.rowA td.bigLetter + td') + page.css('div.jsn-article-content table tbody tr.rowB td.bigLetter + td')
+
+    path_one = 'div.jsn-article-content table tbody tr.row'
+    path_two = 'td.bigLetter'
+    path_three = ' + td'
+
+    word_parse = page.css("#{path_one}A #{path_two}") + page.css("#{path_one}B #{path_two}")
+    description_parse = page.css("#{path_one}A #{path_two} #{path_three}") + page.css("#{path_one}B #{path_two} #{path_three}")
+
     words = Hash[word_parse.zip description_parse]
 
     words.each do |word, definition|
       if definition.text.to_s.length > 2
-        Card.create( original_text: word.text, translated_text: definition.text, review_date: Time.now )
+        Card.create(original_text: word.text, translated_text: definition.text, review_date: Time.now)
       end
     end
 
