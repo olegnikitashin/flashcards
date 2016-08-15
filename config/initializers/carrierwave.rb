7bc93f98d608d6dd57773b8b1d0a9654e9c4e0e9
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-08-15 14:05:04 +0300

    Travis CI 3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,17 +4,6 @@ CarrierWave.configure do |config|
     config.enable_processing = false
     PictureUploader
 
-    config.fog_credentials = {
-      provider:              'AWS',                         # required
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],      # required
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  # required
-      region:                'eu-central-1',                   # optional, defaults to
-    }
-
-    config.fog_directory  = 'onflashcards'                                   # required
-    config.fog_public     = false                                            # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-
     CarrierWave::Uploader::Base.descendants.each do |klass|
       next if klass.anonymous?
       klass.class_eval do
@@ -30,5 +19,15 @@ CarrierWave.configure do |config|
 
   else
     config.storage = :fog
+    config.fog_credentials = {
+      provider:              'AWS',                         # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],      # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  # required
+      region:                'eu-central-1',                   # optional, defaults to
+    }
+
+    config.fog_directory  = 'onflashcards'                                   # required
+    config.fog_public     = false                                            # optional, defaults to true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   end
 end
