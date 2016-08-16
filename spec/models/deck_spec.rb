cfa5e03cc2b6bf4461db426b70a96635194b2c3f
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-08-16 23:50:40 +0300

    Correct deck tests

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 describe Deck do
-  let!(:user) { create :user }
-  let!(:deck) { create(:deck, user: user)}
+  let(:user) { create :user }
+  let(:deck) { create(:deck, user: user)}
   it "becomes current if Make current is selected" do
     deck.make_current
     expect(deck.current).to eq true
   end
 
-  let!(:deck) { create(:deck, user: user) }
-  let!(:deck2) { create(:deck, user: user, title: 'House') }
+  let(:deck2) { create(:deck, user: user, title: 'House') }
   it "becomes current if Make current is selected" do
     deck.make_current
     expect(deck2.current).to eq false
