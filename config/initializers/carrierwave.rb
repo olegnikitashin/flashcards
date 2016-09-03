9f4e50105c7ffa9ea378d5eb691033a19823bff9
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-09-03 19:48:33 +0300

    Change ENVs for deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -21,8 +21,8 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       provider:              'AWS',                         # required
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],      # required
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  # required
+      aws_access_key_id:     Rails.application.secrets.S3_KEY,      # required
+      aws_secret_access_key: Rails.application.secrets.S3_SECRET,  # required
       region:                'eu-central-1',                   # optional, defaults to
     }
 
