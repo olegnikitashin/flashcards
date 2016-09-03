00f56181b50302d713609c2f6224535ffdbf750e
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-09-03 22:35:19 +0300

    Change Sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,18 +76,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'olegnikitashin.com' }
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'on-flashcards.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smpt_settings = {
-  #   address: 'smtp.sendgrid.net',
-  #   port: '587',
-  #   domain: 'heroku.com',
-  #   user_name: ENV['SENGRID_USERNAME'],
-  #   password: ENV['SENDGRID_PASSWORD'],
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  domain: 'olegnikitashin.com',
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: Rails.application.secrets.SENDGRID_USERNAME,
+  password: Rails.application.secrets.SENDGRID_PASSWORD
+  }
 end
-Rails.application.routes.default_url_options[:host] = 'on-flashcards.herokuapp.com'
+Rails.application.routes.default_url_options[:host] = 'olegnikitashin.com'
